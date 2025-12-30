867e3e95c7b448792ccdb76df5c146bc77486ac7
Brandon Brandhuber <brandon.brandhuber@stu.ocsb.ca>
2025-12-30 20:38:34 +0000

    changes

--- vol_sur_area_pyramid.py
+++ vol_sur_area_pyramid.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 # Created by: Brandon
 # Created on: December 19th, 2025
-# This program calculates the volume and surface area of a square pyramid using 2 different functions using a trycatch
+# This program calculates the volume and surface area of a square pyramid
 
 import math
 
-
+# Function to calculate surface area
 def calculate_Surface_Area(s_base, s_height):
     # Calculate and return the surface area of a square pyramid
     surface_area = s_base**2 + 2 * s_base * math.sqrt((s_base**2 / 4) + s_height**2)
     return surface_area
 
-
+# Function to calculate volume
 def calculate_Volume(v_base, v_height):
     # Calculate and return the volume of a square pyramid
     volume = (v_base**2) * v_height / 3
@@ -19,7 +19,7 @@ def calculate_Volume(v_base, v_height):
 
 
 def main():
-    # Get Base and Height from the user and validate input here
+    # Get Base and Height from the user
     try:
         base = float(input("Enter the base of the Square Pyramid: "))
         height = float(input("Enter the height of the Square Pyramid: "))
